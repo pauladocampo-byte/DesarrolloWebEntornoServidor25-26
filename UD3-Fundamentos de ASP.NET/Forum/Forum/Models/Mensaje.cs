ae69ae0ee715e479593d1b3142d6c1a54c501f78
Paula Docampo <pauladocampo@fpfomento.com>
2025-10-14 22:11:13 +0200

    UD3-Actividad 1, Forum , MvcPractica1

---
+++ UD3-Fundamentos de ASP.NET/Forum/Forum/Models/Mensaje.cs
@@ -0,0 +1,8 @@
+﻿namespace Forum.Models
+{
+    public class Mensaje
+    {
+        public string Autor { get; set; } = string.Empty;
+        public string Texto { get; set; } = string.Empty;   
+    }
+}
---
+++ UD3-Fundamentos de ASP.NET/Forum/Forum/Models/MensajeRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace Forum.Models
+{
+    public static class MensajeRepository
+    {
+        private static readonly Dictionary<string, List<Mensaje>> _data =
+          new()
+          {
+              ["asp.net"] = new List<Mensaje>
+              {
+                    new() { Autor="Paula",   Texto="Bienvenidos al foro de ASP.NET" },
+                    new() { Autor="Alumno",  Texto="¿Cómo funciona el routing?" }
+              },
+              ["csharp"] = new List<Mensaje>
+              {
+                    new() { Autor="Profe",  Texto="Repaso de POO: herencia e interfaces" }
+              }
+          };
+        public static IList<Mensaje> GetByForum(string forumName)
+        {
+          
+            forumName = (forumName ?? "").ToLowerInvariant();
+            return _data.TryGetValue(forumName, out var lista)
+                ? lista
+                : new List<Mensaje>();
+        }
+    }
+}
