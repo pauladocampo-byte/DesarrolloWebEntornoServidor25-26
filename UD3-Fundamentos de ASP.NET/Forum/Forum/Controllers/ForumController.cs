15e8143bc167eb2cfaf39447055c3e07d2d4f72c
Paula Docampo <pauladocampo@fpfomento.com>
2025-10-14 22:13:08 +0200

    UD3-Actividad 1, Forum , MvcPractica1 ae69ae0

---
+++ UD3-Fundamentos de ASP.NET/Forum/Forum/Controllers/ForumController.cs
@@ -0,0 +1,19 @@
+﻿using Forum.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.Controllers
+{
+    public class ForumController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Browser(string forumName)
+        {
+            var msgs = MensajeRepository.GetByForum(forumName);
+            return View("Browse", msgs);
+        }
+    }
+}
